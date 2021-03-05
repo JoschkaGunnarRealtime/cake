cb614363f1f788827c600f64bf880350156cc820
Gunnar Magholder <gunnar.magholder@mindmatters.de>
2021-03-05 16:32:01 +0100

    First draft with a fixed torte.. a real fixed torte

---
+++ client/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-container>
+      <Torte />
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import Torte from './components/Torte';
+
+export default {
+  name: 'App',
+
+  components: {
+    Torte,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ client/src/components/Torte.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>Hallo ich bin eine tortenstuecke!</h1>
+        <div 
+            v-for="stueck in tortenstuecke"
+            :key="stueck.url"
+        >
+            <div v-if="stueck.kommentarzettel">
+                <h2>Kommentarzettel</h2>
+                <p>{{ stueck.kommentarzettel }}</p>
+            </div>
+            <div v-else>
+                <h2>tortenstueckenstück</h2>
+                <p> {{ stueck.url }} </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'tortenstuecke',
+    data() {
+        return {
+            tortenstuecke: undefined,
+        };
+    },
+    beforeMount() {
+        this.axios.get('/torte').then((response) => {
+            this.tortenstuecke = response.data.torte.stuecke;
+            console.log(this.tortenstuecke);
+        });
+    }
+}
+</script>
+
+<style>
+</style>
