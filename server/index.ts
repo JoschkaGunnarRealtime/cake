74761a7a0405e4353640d8ed9fc54540b8a536b0
Gunnar Magholder <gunnarm@gmmm.fritz.box>
2021-03-19 14:50:09 +0100

    Mach da ein bisschen Typescript draus

---
+++ server/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+app.use(morgan('combined'));
+app.use(cors());
+
+app.get('/torte', (req: any, res: any) => {
+  res.status(200).json([{
+    torte: {
+      id: 1,
+      stuecke: [
+        { url: 'http://youtube.com/IchBinEinVide0', bedienungsanleitung: 'NSFW' },
+        { url: 'http://youtube.com/IchBinAuchEinVideo', bedienungsanleitung: 'SFW' },
+        { kommentarzettel: 'hm. legger' },
+      ],
+    },
+  }]);
+});
+app.listen(process.env.PORT || 3000);
