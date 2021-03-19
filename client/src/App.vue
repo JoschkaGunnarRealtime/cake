f8d98afebf903dfc3ca93670a3d53fe303681952
gunnar magholder <gunnar.magholder@mindmatters.de>
2021-03-19 16:47:12 +0100

    Push it, aaaaaah

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,28 @@
 <template>
-  <v-app>
-    <v-container>
-      <Torte />
-    </v-container>
-  </v-app>
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import Torte from './components/Torte.vue';
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: 'App',
+#nav {
+  padding: 30px;
 
-  components: {
-    Torte,
-  },
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-  data: () => ({
-    //
-  }),
-};
-</script>
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ client/src/views/Einarbeitstitel.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Torte />
+  </div>
+</template>
+
+<script>
+import Torte from '../components/Torte.vue';
+
+export default {
+  name: 'Einarbeitstitel',
+  components: { Torte },
+};
+</script>
