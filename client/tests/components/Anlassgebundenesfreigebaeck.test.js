ee3755413b417fc8bb641611c2408d3f51351ce7
Gunnar Magholder <gunnar.magholder@mindmatters.de>
2021-07-23 17:33:47 +0200

    Save the test

---
+++ client/tests/components/Anlassgebundenesfreigebaeck.test.js
@@ -0,0 +1,9 @@
+import Anlassgebundenesfreigebaeck from '../../src/components/Anlassgebundenesfreigebaeck.vue';
+import { mount } from '@vue/test-utils';
+
+describe('Anlassgebundenesfreigebaeck', () => {
+    it('should take and compare a snapshot', () => {
+        const wrapper = mount(Anlassgebundenesfreigebaeck);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
---
+++ client/tests/helper/BackwarenZerUndNeuVerbroeslungsTabelle.test.js
@@ -0,0 +1,31 @@
+import BackwarenZerUndNeuVerbroeselungsSystem from '../../src/helper/BackwarenZerUndNeuVerbroeselungsSystem';
+
+describe('BackwarenZerUndNeuVerbroeselungsSystem', () => {
+    describe('#zusammenbroeseln', () => {
+        const backwarenVerschluesselungsTabelle = [
+            ['0', 'Broetchen'],
+            ['1', 'Kranzkuchen'],
+            ['2', 'Lebkuchen'],
+            ['3', 'Franzbroetchen'],
+            ['4', 'Zwieback'],
+            ['5', 'Schokoladenplaetzchen'],
+            ['6', 'Christstollen'],
+            ['7', 'Baklava'],
+            ['8', 'Zimtschnecke'],
+            ['9', 'Cheescake'],
+            ['a', 'Daifuku'],
+            ['b', 'Erdbeerkuchen'],
+            ['c', 'Apfelzimtkuchen'],
+            ['d', 'Melonpan'],
+            ['e', 'Schwarzwaeldertorte'],
+            ['f', 'Diversebackwarenauseuropaunddemrestderwelt']
+        ];
+        it.each(backwarenVerschluesselungsTabelle) ('checks if %s converted to %s', (key, backwarenBenennung) => {
+            expect(BackwarenZerUndNeuVerbroeselungsSystem(key)).toEqual(backwarenBenennung);
+        });
+        
+        it('should omit illegal characters', () => {
+            expect(BackwarenZerUndNeuVerbroeselungsSystem('z')).toEqual('');
+        });
+    });
+});
