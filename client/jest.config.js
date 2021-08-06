d311a628758d06a973879b8e969ad38e25338d4e
JoschkaSchulz <joschka.schulz@mindmatters.de>
2021-08-06 14:29:18 +0200

    working jest tests in client

---
+++ client/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
+    "moduleFileExtensions": [ "js", "ts", "json", "vue" ],
+    transform: {
+      '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
+    },  
+    transformIgnorePatterns: ["node_modules/(?!vue-youtube-embed|vuetify)"]
+  }
